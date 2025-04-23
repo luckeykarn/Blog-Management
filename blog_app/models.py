cd74c0db2399ee1be6c070abb5f64f16b1400511
Luckey Kashyap <you@example.com>
2025-04-23 18:29:50 +0545

    django_projects

---
+++ blog_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post.title}"
+
