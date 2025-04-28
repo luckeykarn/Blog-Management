a3231a03daf15ed7cedabdb0c2eb94a09373361b
Luckey Kashyap <you@example.com>
2025-04-28 18:35:08 +0545

    in account app user login/logout and register templating with functionalatity and blog template,also made urls and views of login,logout,signup and blog

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register,name="signup"),
     path('login/', views.user_login,name="user_login"),
     path('logout/', views.user_logout, name='userlogout'),
-    ## path('profile/', views.profile, name='profile'),
+    # path('profile/', views.profile, name='profile'),
 ]
